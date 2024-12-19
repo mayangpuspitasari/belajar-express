9476f61ab48031b10df9a7340fa81d6c6aae15af
mayangpuspitasari <mayangpuspitas410@gmail.com>
2024-12-19 10:37:22 +0700

    test routh path

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('mantap');
 });
+app.get('/mahasiswa/:nim', (req, res) => {
+  const nim = req.params.nim;
+  res.send(`Mahasiswa berdasarkan Nim : ${nim}`);
+});
 
-app.post('/mahasiswa.post', (req, res) => {
+app.post('/mahasiswa/post', (req, res) => {
   res.send('Ini Postingan Saya');
 });
-app.put('/', (req, res) => {});
-app.delete('/', (req, res) => {});
+app.put('/mahasiswa/put', (req, res) => {
+  res.send('Ini Untuk Update Data');
+});
+app.delete('/mahasiswa/delete', (req, res) => {
+  res.send('ini Untuk Hapus Data');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
